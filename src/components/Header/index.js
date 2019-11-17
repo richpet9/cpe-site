30a96f278deecbb7fa6559677a202a103e06d4ec
richpet9 <richpet9@gmail.com>
2019-11-17 14:04:46 -0500

    header + hero structuring

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './Header.css';
+
 const Header = ({ navItems }) => {
-  return (
-    <header>
-      <nav>
-        <ul>
-          {navItems.map(item => {
-            return (
-              <li key={item.copy}>
-                <a href={item.url} title={item.copy}>
-                  {item.copy}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    </header>
-  );
+    //Get the preferred width from # of items
+    const preferredWidth = Math.floor(100 / navItems.length) + '%';
+
+    return (
+        <header>
+            <nav>
+                <ul>
+                    {navItems.map(item => {
+                        //Render each nav item in a list-item and link
+                        return (
+                            <li key={item.copy} style={{ width: preferredWidth }}>
+                                <a href={item.url} title={item.copy}>
+                                    {item.copy}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 Header.propTypes = {
-  navItems: PropTypes.arrayOf(
-    PropTypes.shape({
-      url: PropTypes.string.isRequired,
-      copy: PropTypes.string.isRequired
-    })
-  ).isRequired
+    //This defines what the navItems prop should look like, and is removed by babel for production
+    navItems: PropTypes.arrayOf(
+        PropTypes.shape({
+            url: PropTypes.string.isRequired,
+            copy: PropTypes.string.isRequired
+        })
+    ).isRequired
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './Root.js';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Home from './pages/Home';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+import './index.css';
+
+function App() {
+    const navItems = [
+        { url: '#', copy: 'Events' },
+        { url: '#', copy: 'About' },
+        { url: '#', copy: 'Services' },
+        { url: '#', copy: 'Join' }
+    ];
+
+    return (
+        <div className="App">
+            <Header navItems={navItems} />
+            <Hero />
+            <Home />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));

1db7644c4cff3693a7161d83ff0d9d011d256fda
richpet9 <richpet9@gmail.com>
2019-12-02 17:18:19 -0500

    backend

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 3008, function() {
+    console.log(`Frontend start on http://localhost:3008`);
+});
